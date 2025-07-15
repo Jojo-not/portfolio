6e6be91f31e5abfa495566f01bb108f063b1342a
Jowen <diezjowenmiguel@gmail.com>
2025-07-15 09:29:40 +0800

    them

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -3,15 +3,19 @@ import React, { useEffect, useState } from 'react'
 import {cn} from '@/lib/utils'
 
 const ThemeToggle = () => {
-   const [isDarkMode, setIsDarkMode] = useState(false);
+   const [isDarkMode, setIsDarkMode] = useState(true);
+
+
 useEffect(()=>{
 const storedTheme = localStorage.getItem('theme')
 if(storedTheme === 'dark'){
-    setIsDarkMode(true);
     document.documentElement.classList.add('dark');
+    setIsDarkMode(true);
+    localStorage.setItem('theme', 'dark');
 }else{
-    setIsDarkMode(false);
      document.documentElement.classList.remove('dark');
+     setIsDarkMode(false);
+
 }
 
 
@@ -22,6 +26,7 @@ if(storedTheme === 'dark'){
         document.documentElement.classList.remove("dark")
         localStorage.setItem('theme','light')
         setIsDarkMode(false);
+        
     }else{
         document.documentElement.classList.add("dark")
         localStorage.setItem('theme','dark')
@@ -33,8 +38,11 @@ if(storedTheme === 'dark'){
     <button onClick={toggleTheme} className={cn('fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300',
         'focus:outline-hidden'
     )}>
-        {isDarkMode ? <Sun className='h-6 w-6 text-yellow-300' /> :
-         <Moon className='h-6 w-6 text-blue-900' /> }
+         {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
     </button>
   )
 }
