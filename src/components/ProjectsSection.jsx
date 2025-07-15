ce5e430ae24846ffa6e68a3f0abc1f207482eb2f
Jowen <diezjowenmiguel@gmail.com>
2025-07-15 10:31:25 +0800

    done

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -72,7 +72,7 @@ const ProjectsSection = () => {
     <section id='projects' className='py-24 px-4 relative'>
         <div className='container mx-auto max-w-5xl'>
             {}
-            <h2 className='text-3xl md:text-4xl font-bold mb-4 text-center'>Featured<span className='text-primary'>projects</span></h2>
+            <h2 className='text-3xl md:text-4xl font-bold mb-4 text-center'>Featured <span className='text-primary'>Projects</span></h2>
         
         <p className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'>
             Here are some of my recent projects. Each project was carefull
